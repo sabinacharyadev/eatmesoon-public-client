b67baaeb4a29962240714a1044438d12cc06686e
sabinbabu <sabinacharya444@gmail.com>
2025-05-25 22:09:19 +1000

    tailwind added

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,9 @@ import "./App.css";
 function App() {
   return (
     <>
-      <p>eat me soon</p>
+      <p className="text-3xl font-bold bg-amber-400">
+        eat me soon - save food, save money, save planet
+      </p>
     </>
   );
 }
